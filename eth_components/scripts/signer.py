33fbdc981001c65c8ba2d22b4435a3c28d1e4959
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-10-30 18:44:09 +0200

    update signer

--- eth_components/scripts/signer.py
+++ eth_components/scripts/signer.py
@@ -2,15 +2,32 @@
 import web3
 from eth_utils import keccak
 
-private_key = "0x78d0030cb696f47a55b378505b8c3054b3f064054a2d0c7531771f71d008b2ca"
-
+with open("keys/keyfile2.key", "r") as f: 
+    private_key = bytes.fromhex(f.read())
+message = bytes.fromhex("5bf1e3244d63b4a4126a055fdd1c046bc464b657d1f24c29dcb56d4a7d0b48ec")
 account = web3.eth.Account.privateKeyToAccount(private_key)
-address = account.address
-message = keccak(bytes(address, 'utf-8'))
+# address = account.address
+# message = keccak(bytes(address, 'utf-8'))
 
-signature = account.sign_message(address)
+signature = account.signHash(message)
 
 print(f"""
-    message={hex(int(message, 16))}
-    signature={hex(int(signature, 16))}
+    message_raw= {message.hex()}
+    message= {signature.messageHash.hex()}
+    signature= {signature.signature.hex()}
+    ----------------------------------------------------
 """)
+
+print(f"""
+    r= {hex(signature.r)}
+    s= {hex(signature.s)}
+    v= {hex(signature.v)}
+""")
+print(len(message.hex()), message.hex())
+print(len(signature.messageHash.hex()), signature.messageHash.hex())
+
+# message_bytes = bytes.fromhex(message)
+# signature_bytes = bytes.fromhex(signature.signature)
+
+recovered_hash = web3.eth.Account.recoverHash(message, signature=signature.signature)
+print(recovered_hash)
