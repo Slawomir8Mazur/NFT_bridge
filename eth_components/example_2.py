a59e95db8cc96e94869b50bc53db29808e555fe2
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-10-30 12:46:56 +0200

    add docstring to example 2

--- eth_components/example_2.py
+++ eth_components/example_2.py
@@ -25,5 +25,10 @@ def _get_signer(operationHash: bytes32, signature: Bytes[65]) -> address:
 
 @external
 def change_signer(operationHash: bytes32, signature: Bytes[65]):
+    """
+    Works for
+    0x6c9c5e133b8aafb2ea74f524a5263495e7ae5701c7248805f7b511d973dc7055
+    0xadcf97303281a870e48d6e1088ae9a2ead8abd18380452e570d736f628c0460c534790de610d494f9bd7022776a2e246ac63b1216bd0ed8d5fba809aa12b09b81c
+    """
     self.signer = self._get_signer(operationHash, signature)
     self.switch = not self.switch
