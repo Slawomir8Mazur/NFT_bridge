938f20af31a733413e6ef8439fbcdd9d53c5bb06
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-11-01 22:06:30 +0100

    refactor, add replay attack protection

--- eth_components/contracts/main.py
+++ eth_components/contracts/main.py
@@ -1,12 +1,11 @@
 from vyper.interfaces import ERC721
 
-# TODO protection from replay atack
-
 event Order:
-    requester: indexed(address)
-    nft_address: indexed(address)
-    nft_id: indexed(uint256)
+    requester: address
+    nft_address: address
+    nft_id: uint256
     target: Bytes[72]
+    hash: bytes32
 
 # Setup signatures
 signers: public(address[5])
@@ -19,7 +18,7 @@ freezer: public(HashMap[bytes32, uint256])
     #     convert(msg.sender, bytes32),
     #     convert(nft_contract, bytes32),
     #     convert(token_id, bytes32))), 
-    # frozen_until=timestamp]
+    # add_time=timestamp]
 
 
 @external
@@ -35,6 +34,7 @@ def __init__():
     self.freezer_period = 1000
 
 
+@view
 @internal
 def _get_signer(message: bytes32, signature: Bytes[65]) -> address:
     return ecrecover(
@@ -44,7 +44,7 @@ def _get_signer(message: bytes32, signature: Bytes[65]) -> address:
         convert(slice(signature, 32, 32), uint256)
     )
 
-
+@view
 @internal
 def _get_valid_signers_count(message: bytes32, signatures: Bytes[65*5]) -> uint256:
     # Verify signatures, deduplicate them
@@ -64,6 +64,23 @@ def _get_valid_signers_count(message: bytes32, signatures: Bytes[65*5]) -> uint2
             confirmation_counter += 1
     return confirmation_counter
 
+@view
+@internal
+def _create_order_hash(owner: address, nft_contract: address, token_id: uint256) -> bytes32:
+    return keccak256(concat(
+        convert(owner, bytes32),
+        convert(nft_contract, bytes32),
+        convert(token_id, bytes32)
+    ))
+
+@view
+@internal
+def _create_order_sign_hash(order_hash: bytes32, add_time:uint256) -> bytes32:
+    return keccak256(concat(
+        order_hash,
+        convert(add_time, bytes32)
+    ))
+
 @external
 def order_migration(nft_contract: address, token_id: uint256, target_account: Bytes[72]):
     """
@@ -74,13 +91,11 @@ def order_migration(nft_contract: address, token_id: uint256, target_account: By
         2. minters will do nothing about it, user will be able to withdraw 
         it after freezing period (up to one hour, it saves us from errors 
         from racing conditions).
+    >>> nft_contract, token_id, target_account ='0x90df8a8ff585d1eb4b9a13789bcc5fd8c7e1b7b2', 18, '0x747a3157487970615735684268625355744b7175746b44745465354a6944434c53704446'
     """
-    self.freezer[keccak256(concat(
-        convert(msg.sender, bytes32),
-        convert(nft_contract, bytes32),
-        convert(token_id, bytes32)
-    ))] = block.timestamp + self.freezer_period
-    log Order(msg.sender, nft_contract, token_id, target_account)
+    order_hash: bytes32 = self._create_order_hash(msg.sender, nft_contract, token_id)
+    self.freezer[order_hash] = block.timestamp
+    log Order(msg.sender, nft_contract, token_id, target_account, order_hash)
 
 
 @external
@@ -89,12 +104,14 @@ def execute_migration(original_owner: address, nft_contract: address, token_id: 
         Minters are minting nft ordered for migration on Tezos, here we will 
         also transfer nft to this contract and remove it from the freezer
     """
-    order_hash: bytes32 = keccak256(concat(
-        convert(msg.sender, bytes32),
-        convert(nft_contract, bytes32),
-        convert(token_id, bytes32)
-    ))
-    assert self.required_signatures <= self._get_valid_signers_count(order_hash, signatures), "Too few valid signatures"
+    order_hash: bytes32 = self._create_order_hash(original_owner, nft_contract, token_id)
+
+    add_time: uint256 = self.freezer[order_hash]
+    assert add_time != empty(uint256), "There was no such an order"
+    assert add_time + self.freezer_period < block.timestamp, "Migration wasn't establised in dued time"
+
+    order_sign_hash: bytes32 = self._create_order_sign_hash(order_hash, add_time)
+    assert self.required_signatures <= self._get_valid_signers_count(order_sign_hash, signatures), "Too few valid signatures"
 
     # Remove order from freezer
     self.freezer[order_hash] = empty(uint256)
@@ -104,19 +121,21 @@ def execute_migration(original_owner: address, nft_contract: address, token_id: 
 
 
 @external
-def cancel_order(original_owner: address, nft_contract: address, token_id: uint256):
+def cancel_order(nft_contract: address, token_id: uint256):
     """
         User can cancel order if minters weren't able to 
         fulfill it in configured timespan
     """
-    order_hash: bytes32 = keccak256(concat(
-        convert(msg.sender, bytes32),
-        convert(nft_contract, bytes32),
-        convert(token_id, bytes32)
-    ))
-    freezing_period: uint256 = self.freezer[order_hash]
-    assert freezing_period != 0, "There was no order for your parameters"
-    assert freezing_period <= block.timestamp, "Freezing period didn't end for that order yet"
+    order_hash: bytes32 = self._create_order_hash(msg.sender, nft_contract, token_id)
+    add_time: uint256 = self.freezer[order_hash]
+    assert add_time != 0, "There was no order for your parameters"
+    assert add_time + self.freezer_period >= block.timestamp, "Freezing period didn't end for that order yet"
 
     # Remove order from the freezer
     self.freezer[order_hash] = empty(uint256)
+
+
+@view
+@external
+def get_order_hash(original_owner: address, nft_contract: address, token_id: uint256) -> bytes32:
+    return self._create_order_hash(original_owner, nft_contract, token_id)
