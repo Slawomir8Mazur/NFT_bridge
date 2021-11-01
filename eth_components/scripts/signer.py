84bfb94b3d3737e8aca8c4302f3158fe92f460b9
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-11-01 22:36:14 +0100

    update signer draft

--- eth_components/scripts/signer.py
+++ eth_components/scripts/signer.py
@@ -19,3 +19,35 @@ print(f"""
     address= {recovered_address}
     ----------------------------------------------------
 """)
+
+def sign_single(message: bytes, keyfile_path: str):
+    with open(keyfile_path, "r") as f: 
+        private_key = bytes.fromhex(f.read())
+    account = web3.eth.Account.privateKeyToAccount(private_key)
+    signature = account.signHash(message)
+    return signature
+
+def concat_signatures(signatures):
+    return "0x" + "".join([
+        s.signature.hex()[2:] for s in signatures
+    ])
+
+def test_n1():
+    """provide 2 signatures from 2 signers, test for to few signatures"""
+    message = bytes.fromhex("5bf1e3244d63b4a4126a055fdd1c046bc464b657d1f24c29dcb56d4a7d0b48ec")
+    signatures = [sign_single(message, f"keys/keyfile{i}.key") for i in range(1,3)]
+    print(concat_signatures(signatures))
+
+def test_n2():
+    """provide 4 signatures from 2 signers, test for duplicates"""
+    message = bytes.fromhex("5bf1e3244d63b4a4126a055fdd1c046bc464b657d1f24c29dcb56d4a7d0b48ec")
+    signatures = [sign_single(message, f"keys/keyfile{i}.key") for i in range(1,3)]
+    [signatures.append(i) for i in signatures.copy()]
+    print(concat_signatures(signatures))
+
+def test_p():
+    message = bytes.fromhex("5bf1e3244d63b4a4126a055fdd1c046bc464b657d1f24c29dcb56d4a7d0b48ec")
+    signatures = [sign_single(message, f"keys/keyfile{i}.key") for i in range(1,5)]
+    print(concat_signatures(signatures))
+
+test_p()
