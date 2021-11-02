d8d48ab9827b5472def9ca88f206165dfef36a1d
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-11-02 22:05:49 +0100

    add unminting, update docstrings

--- eth_components/contracts/main.py
+++ eth_components/contracts/main.py
@@ -17,6 +17,8 @@ freezer: public(HashMap[bytes32, uint256])
 # >>> 0xedc905c6150a64657f27b89836c75ff42924c311ba766e04f12b9169a586ae1c
 # 1635801289
 
+_unmigrate_counter: uint256
+
 
 @external
 def __init__():
@@ -29,7 +31,9 @@ def __init__():
     ]
     self.required_signatures = 3
     self.freezer_period = 3600
+    self._unmigrate_counter = 0
 
+################# PRIVATE METHODS ######################
 
 @view
 @internal
@@ -72,34 +76,52 @@ def _create_order_hash(owner: address, nft_contract: address, token_id: uint256)
 
 @view
 @internal
-def _create_order_sign_hash(order_hash: bytes32, add_time:uint256) -> bytes32:
+def _create_order_sign_hash(order_hash: bytes32, nonce: uint256) -> bytes32:
     return keccak256(concat(
         order_hash,
-        convert(add_time, bytes32)
+        convert(nonce, bytes32)
     ))
+
+######################### USER METHODS ##################################
 
 @external
 def order_migration(nft_contract: address, token_id: uint256, target_account: Bytes[72]):
     """
-        User requests his NFT token on ethereum to be megrated to Tezos, 
-        order is recorded in the freezer, it will either be: 
-        1. minted on tezos and here it will be removed from freezer - this 
-        contract will become a owner of the token
-        2. minters will do nothing about it, user will be able to withdraw 
-        it after freezing period (up to one hour, it saves us from errors 
-        from racing conditions).
+    User requests his NFT token on ethereum to be megrated to Tezos, 
+    order is recorded in the freezer, it will either be: 
+    1. minted on tezos and here it will be removed from freezer - this 
+    contract will become a owner of the token
+    2. minters will do nothing about it, user will be able to withdraw 
+    it after freezing period (up to one hour, it saves us from errors 
+    from racing conditions).
     >>> nft_contract, token_id, target_account ='0x90df8a8ff585d1eb4b9a13789bcc5fd8c7e1b7b2', 18, '0x747a3157487970615735684268625355744b7175746b44745465354a6944434c53704446'
     """
     order_hash: bytes32 = self._create_order_hash(msg.sender, nft_contract, token_id)
     self.freezer[order_hash] = block.timestamp
     log Order(msg.sender, nft_contract, token_id, target_account, order_hash)
 
+@external
+def cancel_order(nft_contract: address, token_id: uint256):
+    """
+    User can cancel order if minters weren't able to 
+    fulfill it in configured timespan
+    """
+    order_hash: bytes32 = self._create_order_hash(msg.sender, nft_contract, token_id)
+    add_time: uint256 = self.freezer[order_hash]
+    assert add_time != 0, "There was no order for your parameters"
+    assert add_time + self.freezer_period >= block.timestamp, "Freezing period didn't end for that order yet"
+
+    # Remove order from the freezer
+    self.freezer[order_hash] = empty(uint256)
+
+######################### VALIDATORS METHODS ##################################
 
 @external
 def execute_migration(original_owner: address, nft_contract: address, token_id: uint256, signatures: Bytes[65*5]):
     """
-        Minters are minting nft ordered for migration on Tezos, here we will 
-        also transfer nft to this contract and remove it from the freezer
+    Token is removed from the freezer, validators confirm with their signatures that they 
+    minted that token on other blockchain, migration can be undone by the seperate procedure 
+    started on other blockchain, freezer no longer applies - user can unmint upon request    
     """
     order_hash: bytes32 = self._create_order_hash(original_owner, nft_contract, token_id)
 
@@ -118,19 +140,19 @@ def execute_migration(original_owner: address, nft_contract: address, token_id: 
 
 
 @external
-def cancel_order(nft_contract: address, token_id: uint256):
+def unmigrate_token(target_owner: address, nft_contract: address, token_id: uint256, signatures: Bytes[65*5]):
     """
-        User can cancel order if minters weren't able to 
-        fulfill it in configured timespan
+    User initiated unmigrating procedure on other blockchain, if it was accepted by validators there, 
+    then here validators should provide their signatures as confirmation - token will be transfered to requested user
     """
-    order_hash: bytes32 = self._create_order_hash(msg.sender, nft_contract, token_id)
-    add_time: uint256 = self.freezer[order_hash]
-    assert add_time != 0, "There was no order for your parameters"
-    assert add_time + self.freezer_period >= block.timestamp, "Freezing period didn't end for that order yet"
+    order_hash: bytes32 = self._create_order_hash(target_owner, nft_contract, token_id)
+    order_sign_hash: bytes32 = self._create_order_sign_hash(order_hash, self._unmigrate_counter)
+    assert self.required_signatures <= self._get_valid_signers_count(order_sign_hash, signatures), "Too few valid signatures"
 
-    # Remove order from the freezer
-    self.freezer[order_hash] = empty(uint256)
+    self._unmigrate_counter += 1
+    ERC721(nft_contract).transferFrom(self, target_owner, token_id)
 
+############################ VIEWS ############################
 
 @view
 @external
@@ -144,6 +166,12 @@ def get_order_hash(original_owner: address, nft_contract: address, token_id: uin
 @view
 @external
 def get_order_sign_hash(original_owner: address, nft_contract: address, token_id: uint256, validate: bool) -> bytes32:
+    """
+    Return hash calculated from: 
+    * `get_order_hash` for token and user ordering migration
+    * nonce that for this function is timestamp when user created an order
+    Function `execute_migration` can only be executed when signatures do sign hash of `this` function 
+    """
     order_hash: bytes32 = self._create_order_hash(original_owner, nft_contract, token_id)
 
     add_time: uint256 = self.freezer[order_hash]
@@ -153,4 +181,15 @@ def get_order_sign_hash(original_owner: address, nft_contract: address, token_id
         assert add_time + self.freezer_period < block.timestamp, "Migration wasn't establised in dued time"
 
     return self._create_order_sign_hash(order_hash, add_time)
-    
+
+@view
+@external
+def get_unmigrate_sign_hash(target_owner: address, nft_contract: address, token_id: uint256) -> bytes32:
+    """
+    Return hash calculated from: 
+    * `get_order_hash` for token and user ordering migration
+    * nonce that for this function is `_unmigrate_counter`
+    Function `unmigrate_token` can only be executed when signatures do sign hash of `this` function 
+    """
+    order_hash: bytes32 = self._create_order_hash(target_owner, nft_contract, token_id)
+    return self._create_order_sign_hash(order_hash, self._unmigrate_counter)
