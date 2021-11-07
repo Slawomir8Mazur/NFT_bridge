f601f118fb0ba07163b77381db76ab63c74f3db6
Slawomir Mazur <slawomir8mazur@gmail.com>
2021-11-07 23:50:55 +0100

    modify signing script

--- validators/contract_sign.py
+++ validators/contract_sign.py
@@ -1,28 +1,26 @@
 import os
 from web3 import Web3
-from utils.contract_wrapper import ContractWrapper
-from utils.signer import EthSigner
+from web3.eth import Eth
+from utils.signer_eth import EthSigner
+from utils.contract_wrapper import EthBridge
 
-    
-contract_wrapper = ContractWrapper(
-    os.path.join('eth_components', 'contracts', 'main.py'), 
-    "0x313309192D561C99563e8cEDcACd8a1655b73879"
+eth_signer = EthSigner("keys/keyfile2.key")
+eth_bridge = EthBridge()
+
+tx = eth_bridge.contract.functions.order_migration(
+    Web3.toChecksumAddress("0x5036bf1c86b03dc74bcf490ea5fbad4426069069"),
+    40,
+    bytes.fromhex("0x747a3157487970615735684268625355744b7175746b44745465354a6944434c53704446".removeprefix('0x'))
+).buildTransaction({
+    'nonce': Eth(eth_bridge.w3).getTransactionCount(eth_signer.public_key),
+    "from": eth_signer.public_key,
+}) 
+
+print(
+    Eth(eth_bridge.w3).getTransactionCount(eth_signer.public_key)
 )
 
-# Load signers
-signers = [EthSigner(os.path.join('keys', f'keyfile{i}.key')) for i in range(1,5)]
-
-order_sign_hash = contract_wrapper.contract.functions.get_order_sign_hash(
-    Web3.toChecksumAddress("0x291Eea42f2806d7b5f14C5e71f3a97b0a5Bcf62e"),
-    Web3.toChecksumAddress("0x5036bf1c86b03dc74bcf490ea5fbad4426069069"),
-    21, 
-    True
-).call()
-
-# Prepare signature for the contract
-signature = EthSigner.concat_signatures([
-    s.sign_hash(order_sign_hash)
-    for s in signers
-])
-
-print(signature.hex())
+print(tx)
+signed_tx = eth_signer.sign_transaction(tx)
+resp = eth_bridge.w3.eth.send_raw_transaction(signed_tx.rawTransaction) 
+print(resp.hex())
